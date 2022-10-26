327e966fee9be12618bb164dba609e447648d683
Dominik <Dominik@DESKTOP-BEL7PCE>
2022-10-26 22:50:00 +0200

    Code cleanup

--- PeopleManager.ViewModel/ApplicationViewModel.cs
+++ PeopleManager.ViewModel/ApplicationViewModel.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using AutoMapper;
 using PeopleManager.Domain.Services;
@@ -10,57 +7,46 @@ using PeopleManager.ViewModel.Abstractions;
 using PeopleManager.ViewModel.Commands;
 using PeopleManager.ViewModel.Models;
 
-namespace PeopleManager.ViewModel
+namespace PeopleManager.ViewModel;
+
+public class ApplicationViewModel : ObservableDto, IViewModel
 {
-    public class ApplicationViewModel : ObservableDto, IViewModel
+    private readonly IMapper _mapper;
+    private readonly IPeopleRepository _peopleRepository;
+    private People _people = new();
+
+    public People People
     {
-
-        private readonly IPeopleRepository _peopleRepository;
-        private readonly IMapper _mapper;
-        private People _people = new();
-
-        public IAsyncCommand LoadPeopleAsyncCommand { get; }
-        public IAsyncCommand SaveCommand { get; }
-        public IAsyncCommand DiscardChangesCommand { get; }
-
-
-        public ApplicationViewModel(IPeopleRepository repository, IMapper mapper)
+        get => _people;
+        set
         {
-            _peopleRepository = repository;
-            _mapper = mapper;
-
-            LoadPeopleAsyncCommand = new AsyncCommand(async () =>
-            {
-                await LoadPeopleAsync();
-            }, ()=>true);
-
-            SaveCommand = new AsyncCommand(async () => { await People.SaveAsync(_mapper, _peopleRepository); },()=> People.CanBeSaved());
-
-            DiscardChangesCommand = new AsyncCommand(async () =>
-            {
-                await LoadPeopleAsync();
-            }, () => People.HaveBeenChanged());
+            if (Equals(value, _people)) return;
+            _people = value;
+            OnPropertyChanged();
         }
-       
-      
-        private async Task LoadPeopleAsync()
-        {
-            var data = await _peopleRepository.GetPeopleAsync();
-            var enumerable = data.ToList();
-            People = new People(_mapper.Map<List<PersonDto>>(enumerable.ToList()));
-          
-        }
+    }
 
+    public IAsyncCommand LoadPeopleAsyncCommand { get; }
+    public IAsyncCommand SaveCommand { get; }
+    public IAsyncCommand DiscardChangesCommand { get; }
 
-        public People People
-        {
-            get => _people;
-            set
-            {
-                if (Equals(value, _people)) return;
-                _people = value;
-                OnPropertyChanged();
-            }
-        }
+    public ApplicationViewModel(IPeopleRepository repository, IMapper mapper)
+    {
+        _peopleRepository = repository;
+        _mapper = mapper;
+
+        LoadPeopleAsyncCommand = new AsyncCommand(async () => { await LoadPeopleAsync(); }, () => true);
+
+        SaveCommand = new AsyncCommand(async () => { await People.SaveAsync(_mapper, _peopleRepository); },
+            () => People.CanBeSaved());
+
+        DiscardChangesCommand =
+            new AsyncCommand(async () => { await LoadPeopleAsync(); }, () => People.HaveBeenChanged());
+    }
+
+    private async Task LoadPeopleAsync()
+    {
+        var data = await _peopleRepository.GetPeopleAsync();
+        People = new People(_mapper.Map<List<PersonDto>>(data.ToList()));
     }
 }
